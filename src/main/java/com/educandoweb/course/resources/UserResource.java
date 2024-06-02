f22fb9a4b02beb978c81538abeedd419c0e58440
Diogo Leonardo <diogoleo57@gmail.com>
2024-06-02 17:33:08 -0300

    User Insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,16 +1,20 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+
+// O @RequestMapping é uma anotação que mapeia solicitações da web
+// para classes manipuladoras específicas, nesse caso, UserResource
+// manipula solicitações da web para /users que realiza operações CRUD
+
 @RequestMapping(value = "/users")
 public class UserResource {
 
@@ -23,10 +27,19 @@ public class UserResource {
         return ResponseEntity.ok().body(list);
     }
 
+    // O método findById é um endpoint para acessar os usuários por id
+    // Metodo GET para obter dados (@GetMapping)
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
-
     }
-}
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+        }
+    }
